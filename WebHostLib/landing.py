37c43f8880584891b400fea289c476fcaa9da6ba
thedrummonger <thedrummonger@DESKTOP>
2026-06-17 14:33:43 -0500

    Block some generation links for non admin users

--- WebHostLib/landing.py
+++ WebHostLib/landing.py
@@ -9,7 +9,6 @@ from .models import Room, Seed
 
 
 @app.route('/', methods=['GET', 'POST'])
-@cache.cached(timeout=300)  # cache has to appear under app route for caching to work
 def landing():
     rooms = count(room for room in Room if room.creation_time >= utcnow() - timedelta(days=7))
     seeds = count(seed for seed in Seed if seed.creation_time >= utcnow() - timedelta(days=7))
